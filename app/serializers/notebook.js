f3d3a0ec2a38d3ef41ec11cb57870dc4f0b259e1
Justin Smestad <justin.smestad@gmail.com>
2015-09-18 13:21:16 -0600

    implement serializer for notebook

--- app/serializers/notebook.js
+++ app/serializers/notebook.js
@@ -1,4 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
+  normalize: function(typeClass, hash, prop) {
+    hash.title = hash.Title;
+    delete hash.Title;
+    hash.user = hash.User;
+    delete hash.User;
+    return this._super(typeClass, hash, prop);
+  },
+  serialize: function(snapshot, options) {
+    var json = {
+      Title: snapshot.attr('title'),
+      User: snapshot.belongsTo('user').id
+    };
+    return json;
+  }
 });

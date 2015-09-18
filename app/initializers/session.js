c8211e69c4fd7de45545a3e026b6a3f438233081
Justin Smestad <justin.smestad@gmail.com>
2015-09-17 21:29:43 -0600

    define session initializer

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -1,5 +1,9 @@
-export function initialize(/* container, application */) {
+export function initialize(container, application) {
   // application.inject('route', 'foo', 'service:foo');
+  var session = Ember.Object.extend();
+  application.register('session:main', session);
+  application.inject('adapter', 'session', 'session:main');
+  application.inject('route', 'session', 'session:main');
 }
 
 export default {
